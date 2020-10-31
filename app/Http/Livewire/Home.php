347ab329484560212012f6fbae74bff23428ba1e
Tiago Matos <tiagovolta@gmail.com>
2020-10-31 15:13:44 -0300

    All libs and configurations

---
+++ app/Http/Livewire/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Home extends Component{
+    
+    public function render(){
+        return view('livewire.home')->extends('layouts.app');;
+    }
+}
---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,5 @@
+<div class="h-full flex items-center justify-center">
+    <div class="w-1/3 bg-white shadow-lg rounded-lg p-5">
+        <h1 class="text-purple-500 text-4xl text-center">Simple Home</h1>
+    </div>
+</div>
